bc081f240b97bdbba3e9d9879a2e248a23280937
Alyona Dyak <miliona19@gmail.com>
2024-02-07 23:54:36 +0100

    updating project with @Scheduled

---
+++ g_27_shop_project/src/main/java/de/aittr/g_27_shop_project/exception_handling/exceptions/SecondTestException.java
@@ -0,0 +1,7 @@
+package de.aittr.g_27_shop_project.exception_handling.exceptions;
+
+public class SecondTestException extends RuntimeException{
+    public SecondTestException(String message) {
+        super(message);
+    }
+}
